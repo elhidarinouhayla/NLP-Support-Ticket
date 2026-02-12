2729f1b9fc7e19b49db894cef362e0596384ce5d
elhidarinouhayla <elhidarinouhayla@gmail.com>
2026-02-12 11:22:16 +0100

    Indexation des embeddings dans ChromaDB

--- utils/embeddings.py
+++ utils/embeddings.py
@@ -18,21 +18,29 @@ def generate_embedding(model, texts):
 
 def normalize_embeddings(embeddings):
 
-    embeddings_nor = embeddings / np.linalg.norm(embeddings)
+    embeddings_nor = embeddings / np.linalg.norm(embeddings, axis=1, keepdims=True)
 
     return embeddings_nor
 
 
-def insexation_chroma(embeddings, df, collection_name="tickets_embeddings"):
+
+def insexation_chroma(embeddings, df, collection_name="tickets_embeddings", batch_size=5000):
 
     client = chromadb.Client()
     collection = client.create_collection(collection_name)
 
-    collection.add(
-        ids=list(df.index),
-        embeddings=embeddings,
-        metadatas=df[['type', 'queue', 'priority']].to_dict(orient='records')
-    )
+    ids = [str(i) for i in df.index]
+    embeddings_list = embeddings.tolist()
+    metadatas = df[['type', 'queue', 'priority', 'language', 'tag_1', 'tag_2', 'tag_3', 'tag_4']].to_dict(orient='records')
 
 
+    total = len(ids)
+    for i in range(0, total, batch_size):
+        end = min(i + batch_size, total)
+        
+        collection.add(
+            ids=ids[i:end],
+            embeddings=embeddings_list[i:end],
+            metadatas=metadatas[i:end]
+        )
     return collection
