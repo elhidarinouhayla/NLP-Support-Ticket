45d5a95c3fc824a4c88205410939ed78e6ded72d
elhidarinouhayla <elhidarinouhayla@gmail.com>
2026-02-12 15:27:15 +0100

    preparation du data pour l'entrainement

---
+++ utils/training.py
@@ -0,0 +1,23 @@
+import joblib 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+
+
+def prepare_data(embeddings, labels):
+
+    le = LabelEncoder()
+    y_encoded = le.fit_transform(labels)
+
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        embeddings,
+        y_encoded,
+        test_size=0.2,
+        random_state=42
+    )
+
+    return x_train, x_test, y_train, y_test
+
+
